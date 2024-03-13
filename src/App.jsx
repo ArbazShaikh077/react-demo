e8c01db083b083e025ed59ff41521b669597b24a
Arbaz Shaikh <shaikharbaz077@gmail.com>
2024-03-13 22:30:29 +0530

    Add key to uniquley identified the component in the list

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
+let id = 1;
 function App() {
-  const [name, nameSetter] = useState("Arbaz");
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      name: "first todo",
+      description: "first description",
+    },
+  ]);
+
   return (
     <>
       <button
         onClick={() => {
-          nameSetter(Math.random());
+          setTodo([
+            ...todo,
+            {
+              id: id++,
+              name: "new name",
+              description: "new description",
+            },
+          ]);
         }}
       >
-        click
+        Click
       </button>
-      <Header title={name} />
-      <Header title="Value" />
+      {todo.map((todo) => (
+        <Todo key={todo.id} name={todo.name} description={todo.description} />
+      ))}
     </>
   );
 }
 
-const Header = React.memo(function Header({ title }) {
-  return <h1>{title}</h1>;
-});
+function Todo(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <br />
+      <h2>{props.description}</h2>
+    </div>
+  );
+}
 export default App;
